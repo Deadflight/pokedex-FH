a538204a911fc04ca56a3d7102d4cd136acb0485
Carlos Correa <correamillancarlos@gmail.com>
2022-03-06 14:13:22 -0400

    [ADD] Layout Principal

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react"
+import Head from "next/head"
+
+interface Props {
+  title?: string
+}
+
+export const Layout: FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || 'Pokemon App'}</title>
+        <meta name="author" content="Carlos Correa"></meta>
+        <meta name="description" content={`Informacion sobre el pokemon ${title}`}></meta>
+        <meta name="keywords" content={`${title}, pokemon, pokedex`}></meta> 
+      </Head>
+      {/* {Navbar} */}
+      <main>
+        {children}
+      </main>
+    </>
+  )
+}
